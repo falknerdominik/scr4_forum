7cb4938a1010043fb4e1bffea9b760981629d8ea
Falkner Dominik <falkner.dominik@gmail.com>
2017-06-23 13:44:14 +0200

    yeP

--- src/BusinessLogic/ContextManager.php
+++ src/BusinessLogic/ContextManager.php
@@ -7,16 +7,8 @@ class ContextManager {
 
     private function __construct() { }
 
-    public static function getInstance() {
-        if(self::$context == null) {
-            self::init_context();
-        }
-        return self::$context;
-    }
-
-    private static function init_context() {
-        // add current user
-        self::$context = new \Domain\Context(
+    public function getInstance() {
+        return new \Domain\Context(
             AuthenticationManager::getAuthenticatedUser(),
             PostManager::getLatestPost(),
            isset($_REQUEST['c']) ? $_REQUEST['c'] : null
